2783ddcd32c0114c91d6942b945750c6b8cdd4b7
suresh2327 <sureshgadi68@gmail.com>
2026-02-21 12:13:21 +0530

    if_else

---
+++ 03_if_else.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl  
+use strict;  #compiler directive to enforce good coding practices
+use warnings; #compiler directive to enable warnings for potential issues in the code
+print "Hello World!\n";
+
+if(1==0){
+    print "conditional statement -1\n";
+    print "conditional statement -2\n";
+}
+else{
+    print "This is else block\n";
+}
+
+#output:
+=start
+Hello World!
+This is else block
+=end
