fe242ef7c79d8111da340744c00cf1b5e0fae88a
suresh2327 <sureshgadi68@gmail.com>
2026-02-21 11:58:41 +0530

    if program

---
+++ 02_if_prog.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl  
+use strict;  #compiler directive to enforce good coding practices
+use warnings; #compiler directive to enable warnings for potential issues in the code
+print "Hello World!\n";
+
+if(1==1){
+    print "conditional statement -1\n";
+    print "conditional statement -2\n";
+}
