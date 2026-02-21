888ef9d8f04f595dcc0be0d0258e1a7984d88605
suresh2327 <sureshgadi68@gmail.com>
2026-02-21 11:56:34 +0530

    Hello world

--- 01_hellworld.pl
+++ 01_hellworld.pl
@@ -1,4 +1,5 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
+#!/usr/bin/perl  
+use strict;  #compiler directive to enforce good coding practices
+use warnings; #compiler directive to enable warnings for potential issues in the code
 print "Hello World!\n";
+
