b9e6d65031b46550c38d9e7775995f79a87b5947
suresh2327 <sureshgadi68@gmail.com>
2026-02-25 10:02:07 +0530

    SCALAR VARIABLES

---
+++ 04_scalar_varible.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;  #compiler directive to enforce good coding practices
+use warnings; #compiler directive to enable warnings for potential issues in the code
+
+
+
+# scalar variable : scalar variable can hold only one value at a time. It can be a string, number or reference.
+print "SCALAR VARIABLE EXAMPLE\n";
+my $name = "suresh"; # scalar variable to hold a string value
+my $age ="21";
+my $height = 5.8; # scalar variable to hold a number value
+my $salary = 5555.55; # scalar variable to hold a number value
+print "Name: $name\n";
+print "Age: $age\n";
+print "Height: $height\n";
+print "Salary: $salary\n";
