b58a7135edbe04976e168ea748993a3c7535f8ad
suresh2327 <sureshgadi68@gmail.com>
2026-02-25 10:26:35 +0530

    ARRAY

--- 05_array_varibale.pl
+++ 05_array_varibale.pl
@@ -10,4 +10,6 @@ print "first student Roll No: $student_names[1]\n"; #print the second element of
 print "Second student name: $student_names[2]\n"; #print the third element of the array variable
 print "second student roll NO: $student_names[3]\n"; #print the fourth element of the array variable
 print "third student name: $student_names[4]\n";
-print "third student roll number: $student_names[5]\n "
+print "third student roll number: $student_names[5]\n ";
+
+print "SIZE OF ARRAY : ".scalar@student_names; # size of array variable
