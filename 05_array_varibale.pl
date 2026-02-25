ddc0a3c48f008acd7a0e16b89490ef72a2892e28
suresh2327 <sureshgadi68@gmail.com>
2026-02-25 10:09:45 +0530

    ARRAYS

---
+++ 05_array_varibale.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @student_names = ("suresh",417,"nikhil",459,"nageshwaro",430);
+
+print"@student_names\n"; #print all the elements of the array variable
+print "First student name: $student_names[0]\n"; #print the first element of the array variable
+print "first student Roll No: $student_names[1]\n"; #print the second element of the array variable
+print "Second student name: $student_names[2]\n"; #print the third element of the array variable
+print "second student roll NO: $student_names[3]\n"; #print the fourth element of the array variable
+print "third student name: $student_names[4]\n";
+print "third student roll number: $student_names[5]\n "
