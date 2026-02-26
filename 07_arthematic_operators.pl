9db846971199d784c7f9c73c73a393b01a07a8ac
suresh2327 <sureshgadi68@gmail.com>
2026-02-26 09:36:53 +0530

    Arthemcatic operators

---
+++ 07_arthematic_operators.pl
@@ -0,0 +1,30 @@
+#arthematic operators
+#!usr/bin/perl
+use strict;
+use warnings;
+
+my $a=10;
+my $b=20;
+
+#arthematic operators are used to perform mathematical operations on numbers. The following are the arthematic operators in perl:
+print "-------Normal print statement-------\n";
+print "Addition: ", $a + $b, "\n"; # addition operator
+print "Subtraction: ", $a - $b, "\n"; # subtraction operator
+print "Multiplication: ", $a * $b, "\n"; # multiplication operator
+print "Division: ", $a / $b, "\n"; # division operator
+print "Modulus: ", $a % $b, "\n"; # modulus operator
+
+print "-------Using concatenation operator-------\n";
+#we can write this print statement using the concatenation operator to make it more readable
+print "Addition: " . ($a + $b) . "\n"; # addition operator
+print "subtraction: " . ($a - $b) . "\n"; # subtraction operator
+print "Multiplication: " . ($a * $b) . "\n"; # multiplication operator
+print "Division: " . ($a / $b) . "\n"; # division operator
+print "Modulus: " . ($a % $b) . "\n"; # modulus operator
+
+
+
+
+
+
+
