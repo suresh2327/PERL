8616e05b067550552ae849ad261032530af6e2d9
suresh2327 <sureshgadi68@gmail.com>
2026-03-01 10:07:03 +0530

    for each loop

---
+++ 24_foreach_loop.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature 'say';
+my @var=("vlsi","perl","python","verilog","systemverilog");
+foreach my $item (@var){
+    say "my item: $item";
+}
+
