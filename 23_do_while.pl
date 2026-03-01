fc806cc14b6d127d9db870c3440899ec6bb996e9
suresh2327 <sureshgadi68@gmail.com>
2026-03-01 10:06:53 +0530

    do while loop

---
+++ 23_do_while.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature 'say';
+#user input from console
+print "Enter a number: ";
+my $var=<STDIN>;
+chomp($var); # remove newline character from the end of the input
+do {
+    say "my var: $var";
+    $var++;
+} while($var<=10);
