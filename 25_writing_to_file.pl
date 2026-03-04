3b12bc04fd9346f35a8d80fcadd0503ca6485f77
suresh2327 <sureshgadi68@gmail.com>
2026-03-04 10:08:47 +0530

    writing data into file

--- 25_writing_to_file.pl
+++ 25_writing_to_file.pl
@@ -6,5 +6,6 @@ open(my $fh, ">", "write.txt") or die "Could not open file";
 #write to file
 print $fh "Hello World\n";
 print $fh "This is a test\n";
+print $fh "this is a program of 25_writing_to_file.pl\n";
 #close file
 close($fh);
