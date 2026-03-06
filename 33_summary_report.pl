ac63109e1a4ae0aeb624d7c1fe96b3a8635285b9
suresh2327 <sureshgadi68@gmail.com>
2026-03-06 09:17:30 +0530

    summary report

---
+++ 33_summary_report.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# open file for reading
+open(my $fh, "<", "info.log") or die "Cannot open file:";
+
+my $error=0;
+my $warning=0;
+my $info=0;
+while (<$fh>){
+    
+    if (/ERROR/){
+        $error++;
+    }
+    if(/WARNING/){
+        $warning++;
+    }
+    if(/INFO/){
+        $info++;
+    }
+}
+
+#status report
+#status for report
+print "------> Status Report <------\n";
+
+if ($error > 0){
+    print "There are $error errors in the log file.\n";
+}
+if ($warning > 0){
+    print "There are $warning warnings in the log file.\n";
+}
+if ($info > 0){
+    print "There are $info info messages in the log file.\n";
+}
+else{
+    print "No errors, warnings, or info messages found in the log file.\n";
+}
+
+#summary report
+print"------> Summary Report <------\n";
+
+print "Total Errors: $error\n";
+print "Total Warnings: $warning\n";
+print "Total Info: $info\n";
+if($error > 0 || $warning > 0 || $info > 0){
+    print "Status: FAILED\n";
+}
+else{
+    print "Status: PASSED\n";
+}
+close($fh);
