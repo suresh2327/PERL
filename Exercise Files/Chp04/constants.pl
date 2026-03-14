1287a54c6dc156ad487f4a0248be17634d9a3d5c
suresh2327 <sureshgadi68@gmail.com>
2026-03-14 09:18:33 +0530

    constants

---
+++ Exercise Files/Chp04/constants.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+
+use 5.18.0;
+use warnings;
+
+use constant PI => 3.141592653589793238462643383279;
+use constant TRUE => 1;
+use constant FALSE => '';
+
+say PI;
+
+if (TRUE) {
+    say 'true';
+} else {
+    say 'false';
+}
+
