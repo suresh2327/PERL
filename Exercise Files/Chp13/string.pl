ec2de4efa20bbb40deaa0f83572ff07b652a63d6
suresh2327 <sureshgadi68@gmail.com>
2026-03-22 11:38:38 +0530

    PERL

--- Exercise Files/Chp13/string.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-# string.pl by Bill Weinman <http://bw.org/contact/>
-
-use 5.18.0;
-use warnings;
-
-my $string = "This string has a bunch of useful characters in it.";
-say $string;
