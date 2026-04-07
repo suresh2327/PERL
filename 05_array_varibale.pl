015261268a5365650766cfa4e6d886e3ea0fd99f
suresh <154115507+suresh2327@users.noreply.github.com>
2026-04-07 09:02:02 +0530

    PERL

--- 05_array_varibale.pl
+++ 05_array_varibale.pl
@@ -11,5 +11,4 @@ print "Second student name: $student_names[2]\n"; #print the third element of th
 print "second student roll NO: $student_names[3]\n"; #print the fourth element of the array variable
 print "third student name: $student_names[4]\n";
 print "third student roll number: $student_names[5]\n ";
-
 print "SIZE OF ARRAY : ".scalar@student_names; # size of array variable
